7b8f9f73c89770fb38df0e3e5f9452678ef7c14e
microwave789 <merei.zhazitova@gmail.com>
2024-03-10 19:02:33 +0500

    add library

--- newsapi/build.gradle.kts
+++ newsapi/build.gradle.kts
@@ -1,9 +1,17 @@
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
+
+
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_7
     targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+dependencies{
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 }
